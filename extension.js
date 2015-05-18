a97c12e5b2f9f48c3b5663741c2a382fc6a4aa29
Slumr <raimond92574@gmail.com>
2015-05-18 14:29:28 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -61,7 +61,7 @@
         startupVolume: 0, // 0-100
         startupEmoji: false, // true or false
         cmdDeletion: true,
-        chatLink: "https://cdn.rawgit.com/Slumr/basicBot/master/lang/en.json",
+        chatLink: "https://rawgit.com/Slumr/basicBot/master/lang/en.json",
         maximumAfk: 120,
         afkRemoval: true,
         maximumDc: 60,
